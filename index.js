50e2c574f5185c1ef55fd5ebcdc0b5fe9c9ffd48
Phacharapol Phukana <phacharapol18@gmail.com>
2022-04-05 14:22:19 -0700

    90% complete

--- index.js
+++ index.js
@@ -8,15 +8,19 @@ const answer2 = document.querySelector('#answer2');
 const answer3 = document.querySelector('#answer3');
 const answer4 = document.querySelector('#answer4');
 let p = document.querySelectorAll('.lista');
-let btn = document.querySelector('#btn')
-
-
+let btn = document.querySelector('#btn');
+let next = document.querySelector('.next');
+const isCorrect = document.querySelector('.iscorrect');
+const reset = document.querySelector('#reset');
 //console.log(p);
 //timer will start
-let count = 15;
+let count = 150;
+let count2 = 1;
+let timer2;
 const timerLeft = document.querySelector('span');
 let timer;
 let isPlay = true;
+
 //questions will change to other questions
 
 
@@ -65,6 +69,51 @@ const questions = [{
     D: 'Toyota'
   },
   rightAnswer: 'Honda'
+},{
+  question: 'When was the Acura NSX first released?',
+  answer: {
+    A: '1980',
+    B: '1995',
+    C: '1990',
+    D: '1991'
+  },
+  rightAnswer: '1990'
+},{
+  question: 'Which of these is NOT a system within a car?',
+  answer: {
+    A: 'Fuel',
+    B: 'Yaw',
+    C: 'Exhaust',
+    D: 'Transmission'
+  },
+  rightAnswer: 'Yaw'
+},{
+  question: '"Alignment" is an aspect of the _____ system.',
+  answer: {
+    A: 'Fuel',
+    B: 'Cooling',
+    C: 'Exhaust',
+    D: 'Steering'
+  },
+  rightAnswer: 'Steering'
+},{
+  question: 'Most modern cars run on a ____-stroke gasoline engine.',
+  answer: {
+    A: '2',
+    B: '3',
+    C: '8',
+    D: '4'
+  },
+  rightAnswer: '4'
+},{
+  question: 'Where does the four-stroke cycle take place?',
+  answer: {
+    A: 'In the carburetor',
+    B: 'In the cylinders',
+    C: 'In the radiator',
+    D: 'In the starter'
+  },
+  rightAnswer: 'In the cylinders'
 }];
 
 
@@ -74,48 +123,140 @@ let currentI = 0;
 
 
 function questionGen() {
-  while (currentI < 5) {
-
-
+  if (document.querySelector('.front')) {
+    questionSec.removeChild(questionSec.firstElementChild);
+  }
+  btn.style.display = 'block';
+  if (currentI < questions.length) {
     question.textContent = `${questions[currentI].question}`;
     answer1.textContent = `${questions[currentI].answer.A}`;
     answer2.textContent = `${questions[currentI].answer.B}`;
     answer3.textContent = `${questions[currentI].answer.C}`;
     answer4.textContent = `${questions[currentI].answer.D}`;
 
-    p.forEach(function (button) {
-      console.log(button);
-      button.addEventListener('click', function () {
+  } else {
+    // function to stop time and pop up score
+   stopTime();
+    //init()
+    renderList()
+  
+  }
 
-        console.log(this.textContent)
-        if (this.textContent !== questions[currentI].rightAnswer) {
+  checkAnswer();
+  
+  isPlay = false
+}
 
-          questionSec.textContent = 'Wrong Answer';
 
-        } else {
-          questionSec.textContent = 'Correct Answer';
-        }
 
-      })
 
-    })
+function handleListeners() {
+
+  console.log(this.textContent)
+  if (this.textContent !== questions[currentI].rightAnswer) {
+    //questionSec.textContent = 'Wrong Answer';
+    isCorrect.textContent = 'Wrong Answer'
+    timer2 = setInterval(function () {
+      count2--;
+      if (count2 <= 0) {
+        clearInterval(timer2)
+        isCorrect.textContent = '';
+      }
+
+    }, 1000);
+
+    count -= 5;
     currentI++;
+    p.forEach(function (button) {
+      button.removeEventListener('click', handleListeners);
+    })
+    questionGen()
+  } else {
+    //questionSec.textContent = 'Correct Answer';
+    isCorrect.textContent = 'Correct Answer'
+    timer2 = setInterval(function () {
+      count2--;
+      if (count2 <= 0) {
+        clearInterval(timer2)
+        isCorrect.textContent = '';
+      }
 
-
-
-
+    }, 1000);
+    count += 10;
+    currentI++;
+    p.forEach(function (button) {
+      button.removeEventListener('click', handleListeners);
+    })
+    questionGen()
   }
 }
 
-questionGen()
+function checkAnswer() {
+  if (currentI === question.length){
+
+   isPlay = false
+
+  } else {
+  p.forEach(function (button) {
+    console.log(button);
+    button.addEventListener('click', handleListeners)
+
+  })
+}
+}
+
+let stopTime = function stop() {
+  clearInterval(timer);
+
+}
+
+function renderList() {
+  
+
+  let user = prompt('Enter your name to see your score');
+  console.log(user)
+  if (document.querySelector('#questionSec')) {
+    questionSec.removeChild(questionSec.firstElementChild);
+    questionSec.removeChild(questionSec.firstElementChild);
+    questionSec.removeChild(questionSec.firstElementChild);
+  
+    var listEl = document.createElement("ol");
+    //listEl.innerHTML = '';
+    listEl.textContent = 'Your Score';
+    questionSec.appendChild(listEl);
+   
+    
+    var scores = JSON.parse(localStorage.getItem("score"))||[];
+    stored(user,scores)
+    scores.forEach(scored =>{
+    var newli = document.createElement("li");
+    newli.textContent =  `${scored.userName.trim()} score is: ${scored.highScore}`;
+    listEl.appendChild(newli)
+})
+    }
+    
+ 
+  
 
 
+  
+  
 
 
+function stored (userName,score){
+  
+  let scoreList = {
+    userName : userName,
+    highScore: count
+  };
+    score.push(scoreList);
+    localStorage.setItem('score',JSON.stringify(score));
+    console.log(score)
+  }
+}
 
 
-
-
+  
 
 
 
@@ -126,7 +267,7 @@ questionGen()
 
 function startGame() {
   if (isPlay) {
-
+    questionGen()
     timer = setInterval(function () {
       count--;
       timerLeft.textContent = count;
@@ -137,11 +278,16 @@ function startGame() {
       }
     }, 1000);
 
-
-  }
+    
+  } 
+  
 }
-
 start.addEventListener('click', startGame);
+reset.addEventListener('click',function (){
+  location.reload();
+})
+
+
 
 //if answer correct 
 // correct answer and generate next question, add time to timer by 5
